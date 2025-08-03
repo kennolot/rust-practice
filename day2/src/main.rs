cc70d0e0dfc9fcd178a974fb7bad701defdf82b5
kennolot <104031547+kennolot@users.noreply.github.com>
2025-08-03 17:14:37 +0300

    day2 commit

---
+++ day2/src/main.rs
@@ -0,0 +1,87 @@
+#[rustfmt::skip]
+/*
+
+Day 2 morning
+
+*/
+
+fn takes_tuple(tuple: (char, i32, bool)) {
+    let a = tuple.0;
+    let b = tuple.1;
+    let c = tuple.2;
+
+    // This does the same thing as above.
+    let (a, b, c) = tuple;
+
+    // Ignore the first element, only bind the second and third.
+    let (_, b, c) = tuple;
+
+    // Ignore everything but the last element.
+    let (.., c) = tuple;
+}
+
+// matching structs
+struct Foo {
+    x: (u32, u32),
+    y: u32,
+}
+
+// matching enums
+enum Result {
+    Ok(i32),
+    Err(String),
+}
+
+fn divide_in_two(n: i32) -> Result {
+    if n % 2 == 0 {
+        Result::Ok(n / 2)
+    } else {
+        Result::Err(format!("cannot divide {n} into two equal parts"))
+    }
+}
+
+use std::time::Duration;
+// if let expression
+fn sleep_for(secs: f32) {
+    let result = Duration::try_from_secs_f32(secs);
+
+    if let Ok(duration) = result {
+        std::thread::sleep(duration);
+        println!("slept for {duration:?}");
+    }
+}
+
+fn main() {
+    takes_tuple(('a', 777, true));
+
+    // matching values
+    let input = 'x';
+    match input {
+        'q'                       => println!("Quitting"),
+        'a' | 's' | 'w' | 'd'     => println!("Moving around"),
+        '0'..='9'                 => println!("Number input"),
+        key if key.is_lowercase() => println!("Lowercase: {key}"),
+        _                         => println!("Something else"),
+    }
+
+    // matching structs
+    let foo = Foo { x: (1, 2), y: 3 };
+    match foo {
+        Foo { y: 2, x: i }   => println!("y = 2, x = {i:?}"),
+        Foo { x: (1, b), y } => println!("x.0 = 1, b = {b}, y = {y}"),
+        Foo { y, .. }        => println!("y = {y}, other fields were ignored"),
+    }
+    // matching enums
+    let n = 100;
+    match divide_in_two(n) {
+        Result::Ok(half) => println!("{n} divided in two is {half}"),
+        Result::Err(msg) => println!("sorry, an error happened: {msg}"),
+    }
+    // if let
+    sleep_for(-10.0);
+    // while let
+    let mut name = String::from("Comprehensive Rust 🦀");
+    while let Some(c) = name.pop() {
+        dbg!(c);
+    }
+}
